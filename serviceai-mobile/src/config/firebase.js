d35d62ad558f9f91d9cf0a62a9be6cc7c238e602
Muaz Aslam <muazaslam1023@gmail.com>
2026-07-21 10:29:24 +0500

    Update Firebase config to use only environment variables
    Removed default values from Firebase configuration to ensure environment variables are used.

--- serviceai-mobile/src/config/firebase.js
+++ serviceai-mobile/src/config/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 import { Platform } from "react-native";
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY || "AIzaSyBfYfONuBzQ_lcDK__zfwXty7dlCjC2KBQ",
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || "hacakathon-service.firebaseapp.com",
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || "hacakathon-service",
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || "hacakathon-service.firebasestorage.app",
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "194501256562",
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID || "1:194501256562:web:79f99300de4904c95929eb",
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-KTR1S48XYY",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Guard against hot-reload re-initialization
